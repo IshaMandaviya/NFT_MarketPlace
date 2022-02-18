460defac48344b10102fec7e88600b08148f986c
isha mandaviya <94522477+IshaMandaviya@users.noreply.github.com>
2022-02-18 17:46:50 +0530

    "adding Test"

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,51 +1,61 @@
-// const { expect } = require("chai");
+const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-// describe("Greeter", function () {
-//   it("Should return the new greeting once it's changed", async function () {
-//     const Greeter = await ethers.getContractFactory("Greeter");
-//     const greeter = await Greeter.deploy("Hello, world!");
-//     await greeter.deployed();
 
-//     expect(await greeter.greet()).to.equal("Hello, world!");
+describe("NFTMarket", function () {
+  it("Should create and execute market sales", async function () {
+    const [deployer, buyer] = await ethers.getSigners();
 
-//     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-//     // wait until the transaction is mined
-//     await setGreetingTx.wait();
-
-//     expect(await greeter.greet()).to.equal("Hola, mundo!");
-//   });
-// });
-describe("NFTMarket", function() {
-    it("Should create and execute market sales", async function() {
-      const [deployer] = await ethers.getSigners();
-  
     const Token = await ethers.getContractFactory("MyToken");
-  const token = await Token.deploy();
+    const token = await Token.deploy();
     console.log("Token address:", token.address);
+    tokenAdress = token.address;
 
     const MyNfts = await ethers.getContractFactory("MyNfts");
     const mynfts = await MyNfts.deploy();
-    console.log("NFT address",mynfts.address);
-	const nftContractAddress=mynfts.address;
+    console.log("NFT address", mynfts.address);
+    const nftContractAddress = mynfts.address;
 
     const NFT_Market_Place = await ethers.getContractFactory("NFT_Market_Place");
     const nft_Market_Place = await NFT_Market_Place.deploy();
-    console.log("NFT MarketPlace address",nft_Market_Place.address);
-	
+    console.log("NFT MarketPlace address", nft_Market_Place.address);
+    nftMarketPlaceAddress = nft_Market_Place.address;
 
+    //create NFT
     await mynfts.createToken("https://gateway.pinata.cloud/ipfs/QmUpW9WoFj4VyYRNkBVACXJSQmUq1eeZW9PCU8LLJn3btM")
     await mynfts.createToken("https://gateway.pinata.cloud/ipfs/QmUpW9WoFj4VyYRNkBVACXJSQmUq1eeZW9PCU8LLJn3btM")
-	// const nftPrice=2000;
-	// let listingPrice=await nft_Market_Place.calculateListingPrice(nftPrice);
-	// listingPrice=listingPrice.toString()
-	// const royalty=250 // 10000 = 1 
-	// await nft_Market_Place.createMarketItem(nftContractAddress, 1, nftPrice, royalty, { value: listingPrice })
-  //   await nft_Market_Place.createMarketItem(nftContractAddress, 2, nftPrice, royalty, { value: listingPrice })
-	// const [_, buyerAddress] = await ethers.getSigners()
 
-  //   await nft_Market_Place.connect(buyerAddress).createMarketSale(nftContractAddress, 1, royalty,{ value: auctionPrice})
-      
-    })
-  })
+    // put NFT In marketPlace
+    const nftPrice = 20000000;
+    const royalty = 250 // 10000 = 1 
+    await nft_Market_Place.createMarketItem(nftContractAddress, 1, nftPrice, royalty)
+    await nft_Market_Place.createMarketItem(nftContractAddress, 2, nftPrice, royalty)
+
+    // Approve nftmarketplace for contract to transfer
+    await mynfts.approve(nftMarketPlaceAddress, 1)
+    await mynfts.approve(nftMarketPlaceAddress, 2)
+
+
+    // Transfer some MYT token to buyer account so it can purchase nft
+    console.log(buyer.address)
+    BuyerAddreass = buyer.address
+    await token.transfer(buyer.address, 100000000000000)
+    await token.connect(buyer).approve(nftMarketPlaceAddress, 10000000000000)
+
+
+
+    // buy NFT from marketplace
+    await nft_Market_Place.connect(buyer).Sale(nftContractAddress, tokenAdress, 1)
+
+    //put NFT for Sale
+    const SalePrice = 200000000
+    await nft_Market_Place.connect(buyer).putTokenForSale(1, SalePrice)
+
+    // Approve nftmarketplace for contract to transfer
+    await mynfts.connect(buyer).approve(nftMarketPlaceAddress, 1)
+
+
+
+
+  }).timeout(300000);
+})
