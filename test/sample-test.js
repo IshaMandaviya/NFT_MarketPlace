f5e03778c532b243576cdd6c3699cd6a77c030e5
isha mandaviya <94522477+IshaMandaviya@users.noreply.github.com>
2022-02-21 11:57:26 +0530

    "Adding task"

--- test/sample-test.js
+++ test/sample-test.js
@@ -23,7 +23,9 @@ describe("NFTMarket", function () {
 
     //create NFT
     await mynfts.createToken("https://gateway.pinata.cloud/ipfs/QmUpW9WoFj4VyYRNkBVACXJSQmUq1eeZW9PCU8LLJn3btM")
+    expect(await mynfts.ownerOf(1)).to.equal(deployer.address);
     await mynfts.createToken("https://gateway.pinata.cloud/ipfs/QmUpW9WoFj4VyYRNkBVACXJSQmUq1eeZW9PCU8LLJn3btM")
+    expect(await mynfts.ownerOf(2)).to.equal(deployer.address);
 
     // put NFT In marketPlace
     const nftPrice = 20000000;
@@ -33,29 +35,35 @@ describe("NFTMarket", function () {
 
     // Approve nftmarketplace for contract to transfer
     await mynfts.approve(nftMarketPlaceAddress, 1)
+    expect(await mynfts.getApproved(1)).to.equal(nftMarketPlaceAddress)
     await mynfts.approve(nftMarketPlaceAddress, 2)
+    expect(await mynfts.getApproved(2)).to.equal(nftMarketPlaceAddress)
+    
 
 
     // Transfer some MYT token to buyer account so it can purchase nft
     console.log(buyer.address)
     BuyerAddreass = buyer.address
     await token.transfer(buyer.address, 100000000000000)
+    expect(await token.balanceOf(buyer.address)).to.equal(100000000000000)
     await token.connect(buyer).approve(nftMarketPlaceAddress, 10000000000000)
-
-
-
+    expect(await token.allowance(buyer.address,nftMarketPlaceAddress)).to.equal(100000000000000)
+    
     // buy NFT from marketplace
+    const StagePrice = 500000;
+    expect(await nft_Market_Place.calculateListingPrice(nftPrice)).to.equal(StagePrice);
     await nft_Market_Place.connect(buyer).Sale(nftContractAddress, tokenAdress, 1)
-
+    expect(await token.balanceOf(nftContractAddress)).to.equal(StagePrice);
+    expect(await token.balanceOf(deployer.address)).to.equal(nftPrice-StagePrice);
+    expect(await mynfts.ownerOf(1)).to.equal(buyer.address);
+     
     //put NFT for Sale
-    const SalePrice = 200000000
+    const SalePrice = 200000000 
     await nft_Market_Place.connect(buyer).putTokenForSale(1, SalePrice)
-
+    
     // Approve nftmarketplace for contract to transfer
     await mynfts.connect(buyer).approve(nftMarketPlaceAddress, 1)
-
-
-
-
+    expect(await mynfts.connect(buyer).getApproved(1)).to.equal(nftMarketPlaceAddress)
+    
   }).timeout(300000);
 })
